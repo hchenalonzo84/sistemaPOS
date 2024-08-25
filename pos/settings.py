c6bbf1e1386116166f6a0a4d137a5d5702f09eb3
Hugo Chenal <hugrishoo@gmail.com>
2024-08-24 22:16:38 -0600

    :sparkles: agregando vista home

--- pos/settings.py
+++ pos/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
         'NAME': 'db_pos',  # Nombre de la base de datos
         'USER': 'postgres',  # Nombre de usuario de la base de datos
         # 'PASSWORD': 'Ubuntu2024$',  # Contraseña de tu usuario PostgreSQL
-        'PASSWORD': '12345',  # Contraseña de tu usuario PostgreSQL
+        # 'PASSWORD': '12345',  # Contraseña de tu usuario PostgreSQL
         'PASSWORD': 'Ubuntu2024$',  # Contraseña de tu usuario PostgreSQL (Hugo)        
-        'PASSWORD': 'Andy10!',  # Contraseña de tu usuario PostgreSQL (Andrea)        
+        # 'PASSWORD': 'Andy10!',  # Contraseña de tu usuario PostgreSQL (Andrea)        
         'HOST': 'localhost',  # Host de la base de datos (normalmente 'localhost')
         'PORT': '5432',  # Puerto de la base de datos (el predeterminado para PostgreSQL es 5432)
     }
