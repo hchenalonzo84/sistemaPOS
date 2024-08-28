14dfb6435a3dbb7f00313be2f2e174c552c30018
Manuel Maquin <manuelzuniga005@gmail.com>
2024-08-27 23:40:03 -0600

    :bulb: Finalización de Categoria

--- inv/models.py
+++ inv/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from bases.models import ClaseModelo
+
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100, #Longitud maxima 100
+        help_text= 'Descripción de la Categoría',
+        unique=True
+    )
+
+    #Hacer referencia a la descripción
+    def __str__(self):
+        return '{}'.format(self.descripcion) #Toma el valor de descripción
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).save() #Invocar el metodo save del padre
+
+    class Meta:
+        verbose_name_plural = "Categorias" #Guardar el nombre del modelo en pluar
